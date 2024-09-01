1f1436f3df324faa5c0e318baa55e1fb1525c36a
Kareem Olamilekan <olamilekankareem717@gmail.com>
2024-09-01 03:33:52 +0100

    Finished the home page. Still lacking mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-// import Signup from './Pages/Signup.jsx';
-// import HomePage from './Pages/Home.jsx'
+import HomePage from './Pages/Home.jsx'
 import Login from './Pages/Login.jsx'
 
 function App() {
     return (
         <div className="App">
-           <Login/>
+           <HomePage/>
 
         </div>
     );
